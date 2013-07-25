8e903ae6eb1e0380770e47ad94e4f495f2da7151
ytboy4 <adamwhite78@googlemail.com>
2013-07-25 14:16:18 +0100

    Add web.js and package.json for heroku

---
+++ web.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var fs = require('fs');
+
+var app = express.createServer(express.logger());
+
+var buffer = new Buffer(fs.readFileSync("index.html"));
+var out = buffer.toString();
+
+app.get('/', function(request, response) {
+	response.send(out);
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+	console.log("Listening on " + port);
+});
